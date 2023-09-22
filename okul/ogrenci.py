25593b11a95391ed3a3366724861d70d301295ee
bilgekavraz <72648959+bilgekavraz@users.noreply.github.com>
2023-09-22 16:16:25 +0300

    Add files via upload

---
+++ okul/ogrenci.py
@@ -0,0 +1,13 @@
+class Ogrenciler :
+    isim = ""
+    soyisim = ""
+    meslek = "Ögrenci"
+    def soruSor(self):
+        print(f"{self.isim} soru soruyor")
+    def sozHakkıIste():
+        return
+    
+    def __init__(self,isim,soyisim,meslek):
+        self.isim = isim
+        self.soyisim = soyisim
+        self.meslek = meslek
